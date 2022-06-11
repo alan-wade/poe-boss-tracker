826abd2e3607acec297ff61fb481c7499146f4e8
alanwade <ajwade1921@gmail.com>
2022-06-11 09:09:17 -0500

    - Updated documentation sources for Swagger to exclude default error handler and group the drop controller better

--- poe-boss-drops/src/main/java/com/poebossdrops/drops/DropController.java
+++ poe-boss-drops/src/main/java/com/poebossdrops/drops/DropController.java
@@ -16,22 +16,26 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/drops")
 @RequiredArgsConstructor
+@Tag(name = "log", description = "Used for interacting with kill logs for users")
 public class DropController {
 
     private final DropService dropService;
 
     @PostMapping("/kill/log")
+    @Tag(name = "log", description = "Logs a kill for a user for the boss was slain")
     public ResponseEntity<LoggedKill> logKill(@RequestBody LoggedKill loggedKill) {
         log.info("Logging record of new kill for bossId " + loggedKill.getBossId().toString());
         return ResponseEntity.ok(dropService.logKill(loggedKill));
     }
 
     @GetMapping("/kill/list/{bossId}")
+    @Tag(name = "log", description = "Gets all the kills and associated drops for a given user and boss")
     public ResponseEntity<List<LoggedKill>> getAllKillsByUserBoss(@PathVariable String bossId, @RequestHeader String appUserId) {
         return ResponseEntity.ok(dropService.getAllKillsByBossUser(bossId, appUserId));
     }
 
     @PostMapping("/kill/log/drop")
+    @Tag(name = "log", description = "Logs a drop for an existing logged kill")
     public ResponseEntity<List<LoggedDrop>> logDrop(@RequestBody LoggedDrop loggedDrop) {
         log.info("Logging record of new kill for loggedKillId " + loggedDrop.getLoggedKillId().toString());
         return ResponseEntity.ok((dropService.logDrop(loggedDrop)));
